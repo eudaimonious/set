98df0e2b2f0fcaac35e2100b82cabf636578ba37
Margaret Scott <margaretscott@patientslikeme.com>
2017-12-07 15:58:50 -0500

    After three cards are selected indicate if they are a match with a blue and red outlines

--- Set/Game.swift
+++ Set/Game.swift
@@ -9,4 +9,24 @@
 import Foundation
 
 class Game {
+    let deck = Deck()
+
+    func hasSet(_ cardIndices: [Int]) -> Bool {
+        let cards = cardIndices.map { deck.cards[$0] }
+        var isSet = true
+
+        let colorSet = Set(cards.map { $0.color })
+        let numberSet = Set(cards.map { $0.number })
+        let shadingSet = Set(cards.map { $0.shading })
+        let symbolSet = Set(cards.map { $0.symbol })
+
+        for set in [colorSet, numberSet, shadingSet, symbolSet] {
+            if set.count == 2 {
+                isSet = false
+                break
+            }
+        }
+        cards.forEach { $0.status = .matched }
+        return isSet
+    }
 }
--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let deck = Deck()
+    lazy var game = Game()
     let cardsToStart = 12
 
     override func viewDidLoad() {
@@ -25,9 +25,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func touchCard(_ sender: UIButton) {
-        let selection = !sender.isSelected
-        sender.isSelected = selection
-        setCardOutline(sender, selected: selection)
+        sender.isSelected = !sender.isSelected
+        let selectedCardButtons = cardButtons.selected
+        if selectedCardButtons.count == 3 {
+            updateMatchIndicatorsFor(selectedCardButtons)
+        } else {
+            let color = sender.isSelected ? #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1) : #colorLiteral(red: 0.6203327134, green: 0.2605122145, blue: 0.7667143085, alpha: 1)
+            setCardOutline(sender,  color)
+        }
     }
 
     @IBAction func touchNewGame(_ sender: UIButton) {
@@ -40,16 +45,12 @@ class ViewController: UIViewController {
     }
 
     private func dealCards(numberOfCards: Int) {
-        let cardsToDeal = deck.nextCards(numberOfCards: numberOfCards)
+        let cardsToDeal = game.deck.nextCards(numberOfCards: numberOfCards)
         for card in cardsToDeal {
-            if let button = findEmptyButton() {
+            if let button = cardButtons.nextEmptyButton {
                 drawCard(button, card)
             }
         }
-    }
-
-    private func findEmptyButton() -> UIButton? {
-        return cardButtons.filter { $0.attributedTitle(for: UIControlState.normal) == nil }.first
     }
 
     let cardColors = [#colorLiteral(red: 0.6203327134, green: 0.2605122145, blue: 0.7667143085, alpha: 1), #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), #colorLiteral(red: 0, green: 0.5603182912, blue: 0, alpha: 1)]
@@ -79,11 +80,29 @@ class ViewController: UIViewController {
         button.setAttributedTitle(NSAttributedString(string: title, attributes: titleAttributes), for: UIControlState.normal)
     }
 
-    private func setCardOutline(_ button: UIButton, selected: Bool = false) {
+    private func setCardOutline(_ button: UIButton, _ color: UIColor = #colorLiteral(red: 0.6203327134, green: 0.2605122145, blue: 0.7667143085, alpha: 1)) {
         button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
-        button.layer.borderColor = selected ? #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1).cgColor : #colorLiteral(red: 0.6203327134, green: 0.2605122145, blue: 0.7667143085, alpha: 1).cgColor
+        button.layer.borderColor = color.cgColor
         button.layer.borderWidth = 3.0
         button.layer.cornerRadius = 8.0
     }
+
+    private func updateMatchIndicatorsFor(_ buttons: [UIButton]) {
+        let selectedCardIndices = buttons.map { cardButtons.index(of: $0)! }
+        if game.hasSet(selectedCardIndices) {
+            buttons.forEach { setCardOutline($0, #colorLiteral(red: 0, green: 0.5603182912, blue: 0, alpha: 1)) }
+        } else {
+            buttons.forEach { setCardOutline($0, #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)) }
+        }
+    }
 }
 
+extension Array where Element: UIButton {
+    var nextEmptyButton: UIButton? {
+        return filter { $0.attributedTitle(for: UIControlState.normal) == nil }.first
+    }
+
+    var selected: [UIButton] {
+        return filter { $0.isSelected }
+    }
+}
